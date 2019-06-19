b784b391740636369c2dc8f684ffccab47fb1517
Jeferson Estevo <jeferson.estevo@summa.com.br>
2019-06-19 19:01:20 -0300

    Implementação da página "home" do usuário

--- user-info-web/src/main/java/br/com/jbd/user/info/config/UserInfoWebMvcConfigurer.java
+++ user-info-web/src/main/java/br/com/jbd/user/info/config/UserInfoWebMvcConfigurer.java
@@ -1,9 +1,13 @@
 package br.com.jbd.user.info.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class UserInfoWebMvcConfigurer implements WebMvcConfigurer {
@@ -25,7 +29,10 @@ public class UserInfoWebMvcConfigurer implements WebMvcConfigurer {
                         "classpath:/META-INF/resources/webjars/",
                         "classpath:/static/img/",
                         "classpath:/static/css/",
-                        "classpath:/static/js/");
+                        "classpath:/static/js/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.DAYS))
+                .resourceChain(false)
+                .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
     }
 
 }
