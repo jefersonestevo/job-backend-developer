77a9fbab0a7d85f6fa44fda0c7811c5bbd888347
Jeferson Estevo <jeferson.estevo@summa.com.br>
2019-06-18 21:53:41 -0300

    Script para criar e popular a base de dados

---
+++ tools/bin/load-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+echo ""
+echo "Loading database scripts"
+echo ""
+
+docker cp ../db/create-db.sql postgres:/tmp
+docker cp ../db/populate-db.sql postgres:/tmp
+
+docker exec postgres /bin/bash -c "
+psql -U postgres -d postgres -a -c \"CREATE DATABASE user_info;\"
+psql -U postgres -d user_info -a -f /tmp/create-db.sql
+psql -U postgres -d user_info -a -f /tmp/populate-db.sql
+exit"
+
+echo ""
+echo "Load database scripts finished"
+echo ""
